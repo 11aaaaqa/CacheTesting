6b6fdee440bb0bd4f3a2ba64280798a3ea493401
aaaaqa <Kvadrocopter@DESKTOP-S5LDS07>
2023-10-03 18:06:12 +0300

    Made the business model public

--- ApplicationCore/Models/Product.cs
+++ ApplicationCore/Models/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ApplicationCore.Models
 {
-    class Product
+    public class Product
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
