085524145fbfa84836bab89a53af77419f569ba0
aaaaqa <Kvadrocopter@DESKTOP-S5LDS07>
2023-10-03 19:15:47 +0300

    added services and middlewares

--- CacheTesting/Program.cs
+++ CacheTesting/Program.cs
@@ -1,10 +1,19 @@
+using ApplicationCore.Interfaces;
+using ApplicationCore.Models;
 using Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
+using Web.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(x => x.UseSqlServer(@"Server=(LocalDB)\MSSQLLocalDB;Database=CacheTesting;Integrated Security=true"));
+builder.Services.AddTransient<IRepository<Product>,ProductRepository>();
+builder.Services.AddMemoryCache();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDeveloperExceptionPage();
+
+app.UseRouting();
+
+app.MapControllerRoute("default", "{controller=Product}/{action=Products}/{id?}");
 
 app.Run();
