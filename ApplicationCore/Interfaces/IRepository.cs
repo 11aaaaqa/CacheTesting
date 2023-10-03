9cf123c0aa5ba7cc4b85bb87370c509ec85266ec
aaaaqa <Kvadrocopter@DESKTOP-S5LDS07>
2023-10-03 18:24:53 +0300

    Added repositry interface

---
+++ ApplicationCore/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IRepository
+    {
+        IEnumerable<T> GetAll<T>();
+        T Get<T>(Guid id);
+        void Add<T>(T entity);
+        void Delete(Guid id);
+        void Edit<T>(T entity);
+    }
+}
