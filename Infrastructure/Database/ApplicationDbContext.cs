8552b153c73a85333ce3bf8effb4ef1a484d2b36
aaaaqa <Kvadrocopter@DESKTOP-S5LDS07>
2023-10-03 18:09:01 +0300

    Added application db context

---
+++ Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationCore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Database
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Product>().HasData(new Product { Id = new Guid("b481ae40-1263-4677-97aa-439b01ca62cb"), Name = "Apple", Price=100});
+        }
+    }
+}
