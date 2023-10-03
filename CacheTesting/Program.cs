8519358174769b9fb0807e29900cbf0d63cc206e
aaaaqa <Kvadrocopter@DESKTOP-S5LDS07>
2023-10-03 18:08:41 +0300

    Added service

--- CacheTesting/Program.cs
+++ CacheTesting/Program.cs
@@ -1,4 +1,8 @@
+using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>(x => x.UseSqlServer(@"Server=(LocalDB)\MSSQLLocalDB;Database=CacheTesting;Integrated Security=true"));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
