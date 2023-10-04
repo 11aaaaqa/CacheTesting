76bc6f83552da8948eca50703736f22e54fbfa19
aaaaqa <Kvadrocopter@DESKTOP-S5LDS07>
2023-10-04 19:54:18 +0300

    Added actions in product controller

--- CacheTesting/Controllers/ProductController.cs
+++ CacheTesting/Controllers/ProductController.cs
@@ -1,12 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApplicationCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Web.Repositories;
 
 namespace Web.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Products()
+        private ProductRepository repository;
+
+        public ProductController(ProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Products()
+        {
+            var products = await repository.GetAll();
+
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult GetProduct()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetProduct(Guid id)
+        {
+            Product product = null;
+            try
+            {
+                product = await repository.Get(id);
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError(string.Empty,e.Message);
+            }
+            
+            return View(product);
         }
     }
 }
